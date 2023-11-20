19ad85a3cc83496b0039c729bbfba61c45e7a48d
Muradyan David <muradyan98@list.ru>
2023-11-20 17:44:09 +0300

    изменение для комментариев

--- resources/views/inc/commentsDiv.blade.php
+++ resources/views/inc/commentsDiv.blade.php
@@ -1,7 +1,7 @@
 <div id="commentsCont" class="hidden">
     @if (!empty($comment))
         @foreach ($comment as $el)
-            <div class="commentCont d-flex flex-column align-items-start mb-3 p-3">
+            <div class="commentCont d-flex flex-column align-items-start mb-3 p-3 hidden-class">
                 <div class="d-flex gap-1 w-100 flex-column">
                     <div class="d-flex gap-3 align-items-center">
                         <div class="rounded-circle comment-img-div">
@@ -20,12 +20,52 @@
             </div>
         @endforeach
     @endif
-    <button type="button" class="btn btn-primary btn-sm comments-btn hidden-class">Small button</button>
+    <button type="button" class="btn btn-primary btn-sm comments-btn hidden-class">Показать еще</button>
 </div>
 
 <script>
     let showMoreBtn = document.getElementsByClassName("comments-btn");
     let commentCard = document.querySelectorAll(".commentCont");
+    let indexCard = 0;
 
     console.dir(commentCard);
+
+    function showMore() {
+        showMoreBtn.addEventlistener("click", () => {
+            cardsShow();
+        })
+    }
+
+    function cardsShow () 
+    {
+        for (let i = indexCard; i < indexCard + 3; i++) 
+        {
+            commentCard[i].classList.remove("hidden-class");
+        }
+
+        if (commentCard.length > indexCard + 3) 
+        {
+            indexCard += 3;
+            showMoreBtn.classList.remove("hidden-class");
+            showMore();
+        } 
+        else if (commentCard.length = indexCard + 3)
+        {
+            indexCard += 3;
+            showMoreBtn.classList.remove("hidden-class");
+            showMore();
+        } 
+        else 
+        {
+            indexCard = commentCard.length - indexCard;
+            showMoreBtn.classList.add("hidden-class");
+            
+            for (let i = indexCard; i < commentCard.length; i++) 
+            {
+                commentCard[i].classList.remove("hidden-class");
+            }
+        }
+    }
+
+    if (commentCard.length > 3) cardsShow();
 </script>
