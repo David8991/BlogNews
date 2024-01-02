3cfd7c771b77c395c614721237a6f26d4a5d228e
Muradyan David <muradyan98@list.ru>
2024-01-02 17:02:47 +0300

    добавили deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "Deploying..."
+git pull origin main
+php8.3 artisan down
+php8.3 composer.phar install --no-dev --optimize=autoloader
+php8.3 artisan migrate --force
+php8.3 artisan config:cach
+php8.3 artisan event:cach
+php8.3 artisan route:cach
+php8.3 artisan view:cach
+php8.3 artisan up
+echo "Done!"
