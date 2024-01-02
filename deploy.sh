651fd5339053a462491f0d4e804f33480ce93e48
Muradyan David <muradyan98@list.ru>
2024-01-02 17:05:54 +0300

    Исправили опечатку

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 echo "Deploying..."
 git pull origin main
 php8.3 artisan down
-php8.3 composer.phar install --no=dev --optimize-autoloader
+php8.3 composer.phar install --no-dev --optimize-autoloader
 php8.3 artisan migrate --force
 php8.3 artisan config:cach
 php8.3 artisan event:cach
